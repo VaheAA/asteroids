e16711d3866988a777a225a362651f63ec602036
VaheAA <vahe.abov@gmail.com>
2024-12-11 22:51:00 +0400

    add some logic

--- main.py
+++ main.py
@@ -1,21 +1,54 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+    
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
+        for u in updatable:
+            u.update(dt)
+            
+        for a in asteroids:
+           is_collision = a.collision(player)       
+           if is_collision:
+                print("Game Over!")
+                sys.exit()
+                
         screen.fill('black')
+        
+        for d in drawable:
+            d.draw(screen)
+            
         pygame.display.flip()
-    
+        
+        dt = clock.tick(60) / 1000
+
     
 if __name__ == "__main__":
     main()  
