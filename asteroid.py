66a126eeed2caf305ba3a5a723b72fbf4d38a847
VaheAA <vahe.abov@gmail.com>
2024-12-11 23:39:42 +0400

    finish game

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
+from constants import *
 import pygame
+import random
 
 from circleshape import CircleShape
 
@@ -12,3 +14,20 @@ class Asteroid(CircleShape):
     def update(self, dt):
         velocity = self.velocity
         self.position += velocity * dt
+        
+    def split(self):
+        self.kill() 
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        random_angle = random.randrange(20, 50)
+        vector_1 = self.velocity.rotate(random_angle)
+        vector_2 = self.velocity.rotate(-random_angle)
+        
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)       
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        
+        asteroid_1.velocity = vector_1 * 1.2
+        asteroid_2.velocity = vector_2 * 1.2
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -16,10 +17,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
-    AsteroidField.containers = (updatable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
@@ -35,10 +38,16 @@ def main():
             u.update(dt)
             
         for a in asteroids:
-           is_collision = a.collision(player)       
-           if is_collision:
+           is_collision_with_player = a.collision(player)       
+           if is_collision_with_player:
                 print("Game Over!")
                 sys.exit()
+                
+           for s in shots:
+               is_collision_hit_by_player = a.collision(s)
+               if is_collision_hit_by_player:
+                   a.split()
+                   s.kill()     
                 
         screen.fill('black')
         
